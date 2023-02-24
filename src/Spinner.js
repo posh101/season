0463e96b61b697d350d9967ccc3b637ee79c3b91
posh101 <paulorife@gmail.com>
2023-02-23 16:22:49 -0800

    My first commit

---
+++ src/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "semantic-ui-css/semantic.min.css";
+
+
+const Spinner = (props) => {
+
+    return (
+        <div className="ui active dimmer">
+            <div className="ui text loader">
+               {props.message}
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
